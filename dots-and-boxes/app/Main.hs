0e78199df5904ea48bfdd0a04c8de4d4dd63dc09
sitongfx <sf3049@columbia.edu>
2021-12-21 21:24:45 -0500

    added main

--- dots-and-boxes/app/Main.hs
+++ dots-and-boxes/app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Lib
 import System.Environment (getArgs, getProgName)
 import System.Exit (die)
+-- import Control.Parallel(par)
 
 main :: IO ()
 main = do
@@ -12,3 +13,11 @@ main = do
     _ -> do
       prog <- getProgName
       die $ "Usage: " ++ prog ++ " <configuration_file> <program-mode>"
+
+-- main :: IO()
+-- main = print (nfib2 0)
+
+-- nfib2 :: Integer -> Integer
+-- nfib2 n | n < 2 = 1
+-- nfib2 n = par nf (nf + nfib2 (n-2) + 1)
+--   where nf = nfib2 (n-1)
